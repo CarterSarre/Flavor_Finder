90b5f0034afe977daabd90bc55a1c4a3c5fc180c
CarterSarre <97630472+CarterSarre@users.noreply.github.com>
2023-05-14 17:07:34 -0400

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/recipe_app/',
+  base: '/Flavor_Finder/',
   plugins: [react()],
 })
